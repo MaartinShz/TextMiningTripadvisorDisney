4358da2d434a23276f5176b31539ea668bcd777f
Christelle KIEMDE <81777234+Esther-Wend@users.noreply.github.com>
2022-12-15 12:10:23 +0100

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 21:52:02 2022
+
+@author: USER
+"""
+
+import streamlit as st
+import os
+import numpy as np
+#os.chdir("C:/Users/USER/Documents/Master_SISE/Projet/Text_mining")
+import pandas as pd
+
+# st.title('''
+         #DisneyLand
+         
+   #      ''')
+     
+         
+st.write('''
+# Bienvenue à DysneyLand
+Cette application permet de..............
+''')
+
+st.sidebar.header("Paramètres")
