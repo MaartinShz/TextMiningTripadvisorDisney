aefbe8ffec066d47b4f32edac526905126f5d6e2
Maartin <maartinschultz@gmail.com>
2022-12-17 16:43:11 +0100

    scrap hotel site and parc site in functions

--- scraper.py
+++ scraper.py
@@ -3,35 +3,17 @@ import requests
 import csv
 
 ############################################ Links
+urlsParc=["https://www.tripadvisor.fr/Attraction_Review-g226865-d189258-Reviews-Disneyland_Paris-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html",
+    "https://www.tripadvisor.fr/Attraction_Review-g226865-d285990-Reviews-Walt_Disney_Studios_Park-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
+    ]
 
-#Tripadvisor Disney Land Paris
-#url="https://www.tripadvisor.fr/Attraction_Review-g226865-d189258-Reviews-Disneyland_Paris-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
-#page2
-#url="https://www.tripadvisor.fr/Attraction_Review-g226865-d189258-Reviews-or10-Disneyland_Paris-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
-# derniere page
-#url = "https://www.tripadvisor.fr/Attraction_Review-g226865-d189258-Reviews-or13890-Disneyland_Paris-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
-
-#Parc Walt Disney Studios
-#"https://www.tripadvisor.fr/Attraction_Review-g226865-d285990-Reviews-Walt_Disney_Studios_Park-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
-
-#Disney Hotel New York - The Art of Marvel
-#"https://www.tripadvisor.fr/Hotel_Review-g1182377-d262678-Reviews-Disney_Hotel_New_York_The_Art_of_Marvel-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_F.html"
-
-#Disney Newport Bay Club
-#"https://www.tripadvisor.fr/Hotel_Review-g1182377-d262679-Reviews-Disney_Newport_Bay_Club-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
-
-#Disney Sequoia Lodge
-#"https://www.tripadvisor.fr/Hotel_Review-g5599092-d262682-Reviews-Disney_Sequoia_Lodge-Coupvray_Seine_et_Marne_Ile_de_France.html"
-
-#Disney Hotel Cheyenne
-#"https://www.tripadvisor.fr/Hotel_Review-g226865-d262686-Reviews-Disney_Hotel_Cheyenne-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
-
-#Disney's Hotel Santa Fe
-#"https://www.tripadvisor.fr/Hotel_Review-g5599092-d262683-Reviews-Disney_Hotel_Santa_Fe-Coupvray_Seine_et_Marne_Ile_de_France.html"
-
-#Disney Davy Crockett Ranch
-#"https://www.tripadvisor.fr/Hotel_Review-g1221082-d564634-Reviews-Disney_Davy_Crockett_Ranch-Bailly_Romainvilliers_Seine_et_Marne_Ile_de_France.html"
-
+urlHotel=["https://www.tripadvisor.fr/Hotel_Review-g1182377-d262678-Reviews-Disney_Hotel_New_York_The_Art_of_Marvel-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_F.html",
+    "https://www.tripadvisor.fr/Hotel_Review-g1182377-d262679-Reviews-Disney_Newport_Bay_Club-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html",
+    "https://www.tripadvisor.fr/Hotel_Review-g5599092-d262682-Reviews-Disney_Sequoia_Lodge-Coupvray_Seine_et_Marne_Ile_de_France.html",
+    "https://www.tripadvisor.fr/Hotel_Review-g226865-d262686-Reviews-Disney_Hotel_Cheyenne-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html",
+    "https://www.tripadvisor.fr/Hotel_Review-g5599092-d262683-Reviews-Disney_Hotel_Santa_Fe-Coupvray_Seine_et_Marne_Ile_de_France.html",
+    "https://www.tripadvisor.fr/Hotel_Review-g1221082-d564634-Reviews-Disney_Davy_Crockett_Ranch-Bailly_Romainvilliers_Seine_et_Marne_Ile_de_France.html"
+    ]
 ############################################ Request
 
 headers = {
@@ -45,36 +27,12 @@ headers = {
 
 
 
-urlsParc=["https://www.tripadvisor.fr/Attraction_Review-g226865-d189258-Reviews-Disneyland_Paris-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html",
-    "https://www.tripadvisor.fr/Attraction_Review-g226865-d285990-Reviews-Walt_Disney_Studios_Park-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html"
-    ]
-
-urlHotel=["https://www.tripadvisor.fr/Hotel_Review-g1182377-d262678-Reviews-Disney_Hotel_New_York_The_Art_of_Marvel-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_F.html",
-    "https://www.tripadvisor.fr/Hotel_Review-g1182377-d262679-Reviews-Disney_Newport_Bay_Club-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html",
-    "https://www.tripadvisor.fr/Hotel_Review-g5599092-d262682-Reviews-Disney_Sequoia_Lodge-Coupvray_Seine_et_Marne_Ile_de_France.html",
-    "https://www.tripadvisor.fr/Hotel_Review-g226865-d262686-Reviews-Disney_Hotel_Cheyenne-Marne_la_Vallee_Seine_et_Marne_Ile_de_France.html",
-    "https://www.tripadvisor.fr/Hotel_Review-g5599092-d262683-Reviews-Disney_Hotel_Santa_Fe-Coupvray_Seine_et_Marne_Ile_de_France.html",
-    "https://www.tripadvisor.fr/Hotel_Review-g1221082-d564634-Reviews-Disney_Davy_Crockett_Ranch-Bailly_Romainvilliers_Seine_et_Marne_Ile_de_France.html"
-    ]
-
-
-"""
-for oneUrl in urls :
-    req = requests.get(urls,headers=headers,timeout=5)
-    if(req.status_code == 200):
-        soup = BeautifulSoup(req.content, 'html.parser')
-        for x in soup.body.find_all(class_="biGQs _P fiohW eIegw"):
-            print(x.text)
-"""
 
 exportAvis = []
-url="https://www.tripadvisor.fr/Hotel_Review-g1182377-d262678-Reviews-Disney_Hotel_New_York_The_Art_of_Marvel-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_F.html"
-i = 0
 ############################################ Scraping Result For Parks
-def scrapParc(url):
-    i=0
-    
-    while(i<100):#url is not None): # condition à changer vérifier date du commmentaire récupérer avec la date de commmentaire le plus récent  
+def scrapParc(url, export=False):
+    i=0#compteur d'avis
+    while(i<20):#url is not None): # condition à changer vérifier date du commmentaire récupérer avec la date de commmentaire le plus récent  
         #time.sleep(3)
         req = requests.get(url,headers=headers,timeout=5)
         #print (req.status_code) # 200 -> OK
@@ -116,58 +74,86 @@ def scrapParc(url):
                 
             ### Enregistrement des données scrapper
             nomColonnes = ["pays", "titre", "note", "commentaire", "dateSejour", "situation", "dateCommentaire", "photo"]
-            print([pays, titre, note, commentaire, dateSejour, situation, dateCommentaire, photo])
             exportAvis.append([pays.replace(",",""), titre.replace(",",""), note, commentaire.replace(",",""), dateSejour, situation, dateCommentaire, photo])
-            
+            print([pays, titre, note, commentaire, dateSejour, situation, dateCommentaire, photo])
             print(i)
-        #########################
+        ######################### next Page
         try:
             url = "https://www.tripadvisor.fr"+blocAvis.find(class_="xkSty").find(class_="BrOJk u j z _F wSSLS tIqAi unMkR")["href"]
         except:
             url = None
+        ############################################ Export result
+        if(export):
+            with open('exportAvisParc'+ soup.title.text.replace(" ","").replace(":","") +'.csv', 'w', encoding='utf-8') as f: 
+                write = csv.writer(f)
+                write.writerow(nomColonnes)
+                write.writerows(exportAvis)
+        ###########################################
 
-############################################ Export result
 
-# with open('exportAvisParcWaltDisney.csv', 'w', encoding='utf-8') as f:
-#     # using csv.writer method from CSV package
-#     write = csv.writer(f)
-#     write.writerow(nomColonnes)
-#     write.writerows(exportAvis)
-############################################
 
 
 ############################################ Scraping Result For Hotel
-req = requests.get(url,headers=headers,timeout=5)
-soup = BeautifulSoup(req.content, 'html.parser')
-blocAvis = soup.find(attrs={"data-test-target" : "reviews-tab"})
-
-for avis in blocAvis.findAll(attrs={"class" : "YibKl MC R2 Gi z Z BB pBbQr"}):
-      
-    pays = avis.find(class_="default LXUOn small").text #PAYS  attention champs à vérifier
-            
-    titre = avis.find(class_="KgQgP MC _S b S6 H5 _a").text #titre commentaire
-    
-    #note = int(avis.find(class_="UctUV d H0")["aria-label"][0]) #note commentaire
-    
-    commentaire = avis.find(class_="fIrGe _T").text #corps du commentaire
-    
-    dateSejour = avis.find(class_="teHYY _R Me S4 H3")#.text #date du séjour
-    #situation =
-    
-    dateCommentaire = avis.find(class_="ui_header_link uyyBf").text #date du commentaire
-    
-    photo = avis.find(class_="_Q t _U s l KSVvt") #Présence de photo: oui/non
-    if(photo is not None):
-        photo = True
-    else:
-        photo = False
+def scrapHotel(url, export = False):
+    i=0  #compteur d'avis
+    while(i<20):
+        req = requests.get(url,headers=headers,timeout=5)
+        soup = BeautifulSoup(req.content, 'html.parser')
         
-    print([pays, titre, note, commentaire, dateSejour, situation, dateCommentaire, photo])
-    
-    print(i)
-    i = i + 1
+        blocAvis = soup.find(attrs={"data-test-target" : "reviews-tab"})
+        
+        for avis in blocAvis.findAll(attrs={"class" : "YibKl MC R2 Gi z Z BB pBbQr"}):
+            i = i + 1
+            if(avis.find(class_="default LXUOn small") is not None):
+                pays = avis.find(class_="default LXUOn small").text 
+            else:
+                pays = ""
+            
+            titre = avis.find(class_="KgQgP MC _S b S6 H5 _a").text #titre commentaire
+            
+            note = avis.find(class_= "Hlmiy F1").find("span")["class"][1][-2] #note commentaire 
+            # récupère balise span qui a comme class avec la note 
+            # la note est l'avant dernier caractère du nom de la classe 
+            
+            commentaire = avis.find(class_="fIrGe _T").text #corps du commentaire
+            
+            #date du séjour # separer date
+            if(avis.find(class_="teHYY _R Me S4 H3") is not None):
+                dateSejour = avis.find(class_="teHYY _R Me S4 H3").text
+            else:
+                dateSejour = ""
+            
+            if(avis.find(class_="TDKzw _R Me") is not None):
+                situation = avis.find(class_="TDKzw _R Me").text
+            else:
+                situation = ""
+            
+            dateCommentaire = avis.find(class_="cRVSd").text #date du commentaire # verif mois et année
+            
+            photo = avis.find(class_="pDrIj f z") #Présence de photo: oui/non 
+            if(photo is not None):
+                photo = True
+            else:
+                photo = False
+                
+            nomColonnes = ["pays", "titre", "note", "commentaire", "dateSejour", "situation", "dateCommentaire", "photo"]
+            exportAvis.append([pays.replace(",",""), titre.replace(",",""), note, commentaire.replace(",",""), dateSejour, situation, dateCommentaire, photo])
+            print([pays, titre, note, commentaire, dateSejour, situation, dateCommentaire, photo])
+            print(i)
+            
+        try:
+            url = "https://www.tripadvisor.fr"+blocAvis.find(class_="ui_button nav next primary")["href"]
+        except:
+            url = None
+        print('exportAvisHotel_'+ soup.title.text.replace(" ","") +'.csv')
+        ############################################ Export result
+        if(export):
+            with open('exportAvisHotel_'+ soup.title.text.replace(" ","").replace(":","") +'.csv', 'w', encoding='utf-8') as f: 
+                write = csv.writer(f)
+                write.writerow(nomColonnes)
+                write.writerows(exportAvis)
+        ###########################################
 
-#print(blocAvis)
-
-
+url="https://www.tripadvisor.fr/Hotel_Review-g1182377-d262678-Reviews-Disney_Hotel_New_York_The_Art_of_Marvel-Chessy_Marne_la_Vallee_Seine_et_Marne_Ile_de_F.html"
+scrapHotel(url, True)
 
