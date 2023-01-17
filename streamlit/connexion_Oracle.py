0167a6523e45457d87c2eb156635ad2ca32c5c37
Esther-Wend <81777234+Esther-Wend@users.noreply.github.com>
2023-01-17 13:26:00 +0100

    version3.4

--- streamlit/connexion_Oracle.py
+++ streamlit/connexion_Oracle.py
@@ -19,6 +19,9 @@ def connect_to_database():
 
     dsn_str = oracledb.makedsn("db-etu.univ-lyon2.fr", "1521", "DBETU")
     con = oracledb.connect(user="m134", password="m134", dsn=dsn_str)
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
     dsn_str = oracledb.makedsn("db-etu.univ-lyon2.fr", "1521", "DBETU")
